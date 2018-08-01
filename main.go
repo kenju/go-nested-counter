a49879ee0e24df34be85dc6ce73e4b2e991d05c7
kenju <ken901waga@gmail.com>
2018-08-01 20:08:13 +0900

    make RootKeyMap public

--- main.go
+++ main.go
@@ -9,32 +9,32 @@ type NestedKey interface {}
 // Counter count incremented value per RootKey and NestedKey.
 // Counter has a mapping table internally to count the values.
 type Counter struct {
-	rootKeyMap map[RootKey]map[NestedKey]Count
+	RootKeyMap map[RootKey]map[NestedKey]Count
 }
 
 // NewCounter returns the reference to a new Counter.
 func NewCounter() *Counter {
 	return &Counter{
-		rootKeyMap: map[RootKey]map[NestedKey]Count{},
+		RootKeyMap: map[RootKey]map[NestedKey]Count{},
 	}
 }
 
 // Increment increment the count value by one for given rootKey and nestedKey.
 // Create a new mapping if the given rootKey/nestedKey pair does not exist yet.
 func (c *Counter) Increment(rootKey RootKey, nestedKey NestedKey, impression Count) {
-	if _, ok := c.rootKeyMap[rootKey]; ok {
-		c.rootKeyMap[rootKey][nestedKey] += impression
+	if _, ok := c.RootKeyMap[rootKey]; ok {
+		c.RootKeyMap[rootKey][nestedKey] += impression
 	} else {
-		c.rootKeyMap[rootKey] = map[NestedKey]Count{nestedKey: impression}
+		c.RootKeyMap[rootKey] = map[NestedKey]Count{nestedKey: impression}
 	}
 }
 
 // GetCount return the count value by given rootKey/nestedKey.
 func (c *Counter) GetCount(rootKey RootKey, nestedKey NestedKey) int {
-	return int(c.rootKeyMap[rootKey][nestedKey])
+	return int(c.RootKeyMap[rootKey][nestedKey])
 }
 
 // Size return the number of RootKey in the mapping table.
 func (c *Counter) Size() int {
-	return len(c.rootKeyMap)
+	return len(c.RootKeyMap)
 }
